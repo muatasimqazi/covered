ed4378d578fe5b25be98f2b293b9684bd62b3156
Michael Park <thatmichaelpark@gmail.com>
2018-02-05 13:41:34 -0800

    fix warning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { app } from './base';
+// import { app } from './base';
 
 class App extends Component {
   render() {
