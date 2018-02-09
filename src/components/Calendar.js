e4d646b6695cfab0a67f81c16e0f65b02856f6c7
Muatasim Qazi <muatasimqazi@gmail.com>
2018-02-08 19:49:37 -0800

    fixed firebase project name in travis.yml

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -4,8 +4,6 @@ import events from './events';
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-
-
 moment.locale('en-GB');
 BigCalendar.momentLocalizer(moment);
 
