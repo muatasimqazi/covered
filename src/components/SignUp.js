66e7b9eacce32537506d7e03ce3dca82ecb45e9d
Michael Park <thatmichaelpark@gmail.com>
2018-02-09 17:05:24 -0800

    signup functionality started

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,13 +1,60 @@
 import React from 'react';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
 
 class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+  handleClick = (info) => {
+    if (info) {
+      
+    }
+    else {
+      this.props.history.push('/');
+    }
+    this.setState({
+      email: '',
+      password: '',
+    });
+  }
   render() {
     return (
       <div>
         <h1>Sign-Up Page</h1>
-        <h1>Sign-Up Page</h1>
-        <h1>Sign-Up Page</h1>
-        <h1>Sign-Up Page</h1>
+        <TextField
+          hintText='Email'
+          name='email'
+          onChange={this.handleChange}
+          value={this.state.email}
+        />
+        <br />
+        <TextField
+          hintText='Password'
+          name='password'
+          onChange={this.handleChange}
+          value={this.state.password}
+        />
+        <br />
+        <FlatButton
+          label="Cancel"
+          primary={true}
+          onClick={() => this.handleClick(null)}
+        />,
+        <FlatButton
+          label="Submit"
+          primary={true}
+          onClick={() => this.handleClick(this.state)}
+        />,
       </div>
     );
   }
