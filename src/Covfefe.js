b0ca0e87298dab75e811b770a791a5433101751c
Michael Park <thatmichaelpark@gmail.com>
2018-02-18 15:46:12 -0800

    logged in/out

--- src/Covfefe.js
+++ src/Covfefe.js
@@ -1,39 +1,164 @@
 import { app } from './base';
-import { observable } from 'mobx';
+import { toJS, computed, observable } from 'mobx';
+
+const auth = app.auth();
+let db = app.database();
 
 class Covfefe {
   @observable email = '';
   @observable isLoggedIn = false;
   @observable uid = '';
+  @observable employeesObj = {};
+  @observable supervisorsObj = {};
+  @computed get currentUser() {
+    console.log('get', this.isLoggedIn);
+    // if (!this.isLoggedin) {
+    //   return {};
+    // }
+    console.log('emps', toJS(this.employeesObj));
+    for (let id in this.employeesObj) {
+      console.log(id, this.employeesObj[id].email, this.email);
+      if (this.employeesObj[id].email === this.email) {
+        return this.employeesObj[id];
+      }
+    }
+    console.log('supes', toJS(this.supervisorsObj));
+    for (let id in this.supervisorsObj) {
+      console.log(id, this.supervisorsObj[id].email, this.email);
+      if (this.supervisorsObj[id].email === this.email) {
+        return this.supervisorsObj[id];
+      }
+    }
+    return {};
+  }
   constructor() {
-    // real-time authentication listener
-    app.auth().onAuthStateChanged(firebaseUser => {
+    // real-time listeners
+    auth.onAuthStateChanged(firebaseUser => {
       if (firebaseUser) {
         this.isLoggedIn = true;
         this.email = firebaseUser.email;
         this.uid = firebaseUser.uid;
+        console.log('email', this.email, this.isLoggedIn);
+        db.ref('test/employees').on('value', (snapshot, error) => {
+          if (error) {
+            console.log('Error:', error);
+            return;
+          }
+          console.log('empsObj', snapshot.val());
+          this.employeesObj = observable(snapshot.val() || {});
+        });
+        db.ref('test/supervisors').on('value', (snapshot, error) => {
+          if (error) {
+            console.log('Error:', error);
+            return;
+          }
+          console.log('supesObj' ,snapshot.val());
+          this.supervisorsObj = observable(snapshot.val() || {});
+        });
       }
       else {
         this.isLoggedIn = false;
         this.email = '';
       }
     });
+
   }
-  LogIn(email, password) {
+  logIn(email, password) {
     console.log('log in');
-    app.auth().signInWithEmailAndPassword(email, password)
+    auth.signInWithEmailAndPassword(email, password)
     .catch(error => {
       this.loggedIn = false;
       this.email = '';
       console.log('Error', error.message);
     });
   }
-  LogOut() {
-    app.auth().signOut()
+  logOut() {
+    auth.signOut()
     .catch(error => {
       console.log('Error', error.message);
     });
   }
+  resetDb() {
+    db.ref('test').set(null);
+    const team1Id = db.ref(`test/teams`).push({ teamName: 'Loss Leaders'}).key;
+    const team2Id = db.ref(`test/teams`).push({ teamName: 'Money Makers'}).key;
+    const employees = [{
+      "email": "lars@levine.com",
+      "firstName": "Lars",
+      "lastName": "Levine",
+      "phone": "453",
+      "role": "employee",
+      teamId: team1Id
+    }, {
+      "email": "lana@linden.com",
+      "firstName": "Lana",
+      "lastName": "Linden",
+      "phone": "89098",
+      "role": "employee",
+      teamId: team1Id
+    }, {
+      "email": "logan@lock.com",
+      "firstName": "Logan",
+      "lastName": "Lock",
+      "phone": "20893",
+      "role": "employee",
+      teamId: team1Id
+    }, {
+      "email": "Laraine@Lilly.com",
+      "firstName": "Laraine",
+      "lastName": "Lilly",
+      "phone": "",
+      "role": "employee",
+      teamId: team1Id
+    }, {
+      "email": "mary@meta.com",
+      "firstName": "Mary",
+      "lastName": "Meta",
+      "phone": "1489098",
+      "role": "employee",
+      teamId: team2Id
+    }, {
+      "email": "morgan@matlock.com",
+      "firstName": "Morgan",
+      "lastName": "Matlock",
+      "phone": "4520893",
+      "role": "employee",
+      teamId: team2Id
+    }, {
+      "email": "maureen@milly.com",
+      "firstName": "Maureen",
+      "lastName": "Milly",
+      "phone": "543",
+      "role": "employee",
+      teamId: team2Id
+    }];
+    const supervisors = [{
+      "email": "lisa@leslie.com",
+      "firstName": "Lisa",
+      "lastName": "Leslie",
+      "phone": "7890",
+      "role": "supervisor",
+      teamId: team1Id
+    }, {
+      "email": "mark@measly.com",
+      "firstName": "Mark",
+      "lastName": "Measly",
+      "phone": "547890",
+      "role": "supervisor",
+      teamId: team2Id
+    }];
+    employees.forEach(employee => {
+      const ref = db.ref(`test/employees`).push();
+      employee.id = ref.key;
+      ref.set(employee);
+    });
+    supervisors.forEach(supervisor => {
+      const ref = db.ref(`test/supervisors`).push();
+      supervisor.id = ref.key;
+      ref.set(supervisor);
+    });
+  }
 }
 
-export default new Covfefe();
+const covfefe = new Covfefe();
+export default covfefe;
--- src/components/Blah.js
+++ src/components/Blah.js
@@ -4,38 +4,66 @@ import { observer } from 'mobx-react';
 
 @observer
 class Blah extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  componentDidMount() {
-    covfefe.LogIn('lisa@leslie.com', 'covered');
-    // app.database().ref('users').on('value', (snapshot, error) => {
-    //   if (error) {
-    //     console.log('Error:', error);
-    //     return;
-    //   }
-    //   // const val = snapshot.val() || {};
-    //   // const keys = Object.keys(val);
-    //   // const users = [];
-    //   // keys.forEach(key => {
-    //   //   users.push(val[key]);
-    //   //   console.log(val, key, val[key]);
-    //   // });
-    //   // console.log(users);
-    //   // this.setState({ users });
-    // });
-  }
-  
   render() {
     return (
       <div>
-        <h1>User!</h1>
-        {covfefe.isLoggedIn? 'Logged in' : 'Logged out'}
-        {/* <ul>
-          {this.state.users.map(user => 
-            <li key={user.email}>{user.firstName} {user.lastName}</li>
-          )}
-        </ul> */}
+        {/* {covfefe.email} */}
+        {covfefe.isLoggedIn? <LoggedIn/> : <LoggedOut/>}
+      </div>
+    );
+  }
+}
+
+@observer
+class LoggedOut extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: 'lars@levine.com',
+      password: 'covered'
+    };
+  }
+  handleChange = (e) => {
+    this.setState({ [[e.target.name]]: e.target.value });
+  }
+  handleClick = (e) => {
+    covfefe.logIn(this.state.email, this.state.password);
+  }
+  render() {
+    return (
+      <div>
+        <input name="email" onChange={this.handleChange} value={this.state.email}/>
+        <input name="password" onChange={this.handleChange} type="password" value={this.state.password}/>
+        <button name="ok" onClick={this.handleClick}>OK</button>
+      </div>
+    );
+  }
+}
+
+@observer
+class LoggedIn extends React.Component {
+  handleClick = (e) => {
+    if (e.target.name === 'logout') {
+      covfefe.logOut();
+    }
+    else if (e.target.name === 'resetdb') {
+      covfefe.resetDb();
+    }
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>First name: {covfefe.currentUser.firstName}</li>
+          <li>Last name: {covfefe.currentUser.lastName}</li>
+          <li>Email: {covfefe.currentUser.email}</li>
+          <li>ID: {covfefe.currentUser.id}</li>
+          <li>Role: {covfefe.currentUser.role}</li>
+          <li>Team: {covfefe.currentUser.teamId}</li>
+          <li>{covfefe.isLoggedIn.toString()}</li>
+        </ul>
+        <button name="logout" onClick={this.handleClick}>Log out</button>
+        <button name="resetdb" onClick={this.handleClick}>Reset DB</button>
       </div>
     );
   }
