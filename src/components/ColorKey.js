0dcb4d73f24bfc39c62555fcac42c717a9c078ff
Muatasim Qazi <muatasimqazi@gmail.com>
2018-03-15 05:10:03 -0700

    adjust calender color according to ui palette

--- src/components/ColorKey.js
+++ src/components/ColorKey.js
@@ -12,39 +12,46 @@ const styles = {
 class ColorKey extends React.Component {
   render() {
     const chipData = [{
-      color: '#99e',
-      bkgdColor: '#bbf',
+      color: '#FFF',
+      bkgdColor: 'rgb(0, 145, 234)',
       avatarIcon: 'help_outline',
-      text: 'Too much coverage!'
+      text: 'Too much coverage!',
+      border: '1px solid rgb(0, 145, 234)',
     }, {
-      color: '#9e9',
-      bkgdColor: '#bfb',
+      color: '#FFF',
+      bkgdColor: 'rgb(56, 142, 60)',
       avatarIcon: 'done',
-      text: '100% coverage'
+      text: '100% coverage',
+      border: '1px solid rgb(56, 142, 60)',
     }, {
-      color: '#dd8',
-      bkgdColor: '#ffb',
+      color: '#FFF',
+      bkgdColor: 'rgb(255, 167, 38)',
       avatarIcon: 'error_outline',
-      text: 'Understaffed'
+      text: 'Understaffed',
+      border: '1px solid rgb(255, 167, 38)',
     }, {
-      color: '#faa',
-      bkgdColor: '#fbb',
+      color: '#FFF',
+      bkgdColor: 'rgb(255, 87, 34)',
       avatarIcon: 'error',
-      text: 'Severely understaffed!'
+      text: 'Severely understaffed!',
+      labelColor: '#FFF',
+      border: '1px solid rgb(255, 87, 34)',
     }];
     return (
       <div>
-        {chipData.map((chip, i) => 
+        {chipData.map((chip, i) =>
           <Chip
             backgroundColor={chip.bkgdColor}
             key={i}
             style={styles.chip}
           >
-            <Avatar color={'#fff'} backgroundColor={chip.color}>
+
+            <Avatar color={'#fff'} backgroundColor={chip.color} style={{ border: chip.border }}>
               <FontIcon className="material-icons" style={styles.icons}>{chip.avatarIcon}</FontIcon>
             </Avatar>
-            {chip.text}
-          </Chip>      
+            <div style={{ color: '#FFF' }}>{chip.text} </div>
+
+          </Chip>
         )}
       </div>
     );
